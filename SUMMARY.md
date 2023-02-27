d768927df56fbaea1045fcb2bbf79a964d05ebcb
magician <2218590381@qq.com>
2023-02-27 11:40:56 +0800

    first

---
+++ SUMMARY.md
@@ -0,0 +1,2 @@
+- [消息总线](xiaoxi/README.md)
+    - [子章节](xiaoxi/zi1/md)
---
+++ book.js
@@ -0,0 +1,23 @@
+module.export={
+    // 书籍信息
+    "title": "Book-Flutter",
+    "description": "前端学习Flutter指南",
+    "isbn": "图书编号",
+    "author": "Magician",
+    "lang": "zh-cn",
+
+    // 插件列表
+    "plugins": ["-lunr","-search","search-pro","code","expandable-chapters","back-to-top-button","flexible-alerts"],
+
+    // 插件全局配置
+    "pluginsConfig": {
+        "flexible-alerts": {
+            style: "flat" // callout 或 flat
+        }
+    },
+
+      // 模板变量
+      "variables": {
+          // 自定义
+    }
+}
